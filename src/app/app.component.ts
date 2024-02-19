c02baed231d6a9695698ff86dc7cf0b070c01baa
Coudji <coudji@ymail.com>
2024-02-19 16:34:13 +0100

    fin jour 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,23 @@ import { SuperComponentsComponent } from './super-components/super-components.co
 import { ZippyComponent } from './zippy/zippy.component';
 import { ZippyComponent2 } from './zippy2/zippy2.component';
 import { BananaInABoxComponent } from './banana-in-a-box/banana-in-a-box.component';
+import { TrucComponent } from './truc/truc.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SuperComponentsComponent, ZippyComponent, ZippyComponent2, BananaInABoxComponent],
+  imports: [RouterOutlet, SuperComponentsComponent, ZippyComponent, ZippyComponent2, BananaInABoxComponent, TrucComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'introduction';
+
+  alertTruc(value: string){
+    alert(value);
+  }
+
+  consoleTruc(value: string){
+    console.log(value);
+  }
 }
--- src/app/truc/truc.component.ts
+++ src/app/truc/truc.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-truc',
   standalone: true,
-  imports: [],
-  templateUrl: './truc.component.html',
+  imports: [FormsModule],
+  template: `
+    <input type="text" name="truc" [(ngModel)]="inputValue" (keyup.enter)="fireTruc()">
+  `,
   styleUrl: './truc.component.sass'
 })
 export class TrucComponent {
-
+  inputValue="";
+  @Output() trucEmitter = new EventEmitter();
+  fireTruc(){
+    console.log('fired');
+    this.trucEmitter.emit(this.inputValue)
+  }
 }
